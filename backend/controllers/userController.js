2846026f13594136f982bfd39b58f28585e8806e
tanvirtbf <tanver018765@gmail.com>
2024-11-07 15:47:38 +0600

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -186,6 +186,32 @@ const userLogin = async (req,res)=> {
 
 
 // Get New Access Token OR Refresh Token
+const getNewAccessToken = async (req,res) => {
+  try {
+    // Get new access token using Refresh Token
+    const { newAccessToken, newRefreshToken, newAccessTokenExp, newRefreshTokenExp } = await refreshAccessToken(req, res)
+
+    // Set New Tokens to Cookie
+    setTokensCookies(res, newAccessToken, newRefreshToken, newAccessTokenExp, newRefreshTokenExp)
+
+    res.status(200).send({
+      status: "success",
+      message: "New tokens generated",
+      access_token: newAccessToken,
+      refresh_token: newRefreshToken,
+      access_token_exp: newAccessTokenExp
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "failed", message: "Unable to generate new token, please try again later" });
+  }
+}
+
+
+
+
+
 
 // Change Password
 
