1d762b41f100b26dea5a6f602af0f193f5492a7b
tanvirtbf <tanver018765@gmail.com>
2024-11-09 02:46:48 +0600

    Access token auto refresh functionality added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,28 @@
-import express from 'express'
-import { userLogin, userProfile, userRegistration, verifyEmail } from '../controllers/userController.js'
-import refreshAccessToken from '../utils/refreshAccessToken.js'
-import passport from 'passport'
-import setAuthHeader from '../middlewares/setAuthHeader.js'
+import express from "express";
+import {
+  userLogin,
+  userProfile,
+  userRegistration,
+  verifyEmail,
+} from "../controllers/userController.js";
+import refreshAccessToken from "../utils/refreshAccessToken.js";
+import passport from "passport";
+import accessTokenAutoRefresh from "../middlewares/accessTokenAutoRefresh.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // Public Routes
-router.post('/register', userRegistration)
-router.post('/verify-email', verifyEmail)
-router.post('/login', userLogin)
-router.post('/refresh-token', refreshAccessToken)
+router.post("/register", userRegistration);
+router.post("/verify-email", verifyEmail);
+router.post("/login", userLogin);
+router.post("/refresh-token", refreshAccessToken);
 
 // Protected Routes
-router.get('/me', setAuthHeader, passport.authenticate('jwt', { session: false }), userProfile)
+router.get(
+  "/me",
+  accessTokenAutoRefresh,
+  passport.authenticate("jwt", { session: false }),
+  userProfile
+);
 
-export default router
+export default router;
